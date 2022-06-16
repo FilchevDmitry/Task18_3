d3d57f06f9fe0c0d716d2072690df88d208effdb
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-06-16 19:28:35 +0300

    Добавьте файлы проекта.

---
+++ Task18_3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int jamp(int n, int k)
+{   
+    int num = 0;
+    if (n == 0)
+        return 1;
+    for (int i = 1; i <= k; i++)
+    {
+        if (i <= n)
+            num += jamp(n - i, k);
+    }
+    return num;
+}
+int main()
+{
+    int n = 0;
+    int k;
+    std::cout << "Enter the number of steps : ";
+    std::cin >> n;
+    std::cout << "Enter the maximum jump length : ";
+    std::cin >> k;
+    if (k <= 0)
+        k = 3;
+    std::cout << jamp(n,k);
+}
